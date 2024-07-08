6357c194c82ceee74e2fbf4fac1123b9d25fb15a
jiwon <jiwon0768@tukorea.ac.kr>
2024-07-09 00:06:19 +0900

    [feat] spring boot 환경설정

---
+++ jett/src/main/java/com/choandyoo/jett/JettApplication.java
@@ -0,0 +1,13 @@
+package com.choandyoo.jett;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JettApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JettApplication.class, args);
+	}
+
+}
