857a2bf19700dde6d7c51cbc9c0c55849714b637
choyunju <yoonj1860@naver.com>
2025-01-06 15:35:06 +0900

    [feat] 사용자 추가 수락, 거절 기능 구현

--- src/main/java/com/choandyoo/jett/invitation/controller/InvitationController.java
+++ src/main/java/com/choandyoo/jett/invitation/controller/InvitationController.java
@@ -1,10 +1,12 @@
 package com.choandyoo.jett.invitation.controller;
 
 import com.choandyoo.jett.common.CustomApiResponse;
+import com.choandyoo.jett.config.CustomUserDetails;
 import com.choandyoo.jett.invitation.servie.InvitationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -22,4 +24,18 @@ public class InvitationController {
         return ResponseEntity.status(HttpStatus.CREATED).body(CustomApiResponse.onSuccess(inviteUrl));
     }
 
+    @PostMapping("/{travelId}/{invitation}/response")
+    public ResponseEntity<CustomApiResponse<String>> respondToInvite(
+            @PathVariable("travelId") Long travelId,
+            @PathVariable("invitation") String invitation,
+            @RequestParam("status") boolean accept,
+            @AuthenticationPrincipal CustomUserDetails customUserDetails)
+    {
+        if(accept) {
+            invitationService.respondToInvite(travelId, invitation, customUserDetails);
+        } else {
+            return ResponseEntity.status(HttpStatus.OK).body(CustomApiResponse.onSuccess(customUserDetails.getUsername() + "님이 초대를 거절하였습니다."));
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(CustomApiResponse.onSuccess(customUserDetails.getUsername() +"님이 성공적으로 추가되었습니다."));
+    }
 }
--- src/main/java/com/choandyoo/jett/invitation/servie/InvitationService.java
+++ src/main/java/com/choandyoo/jett/invitation/servie/InvitationService.java
@@ -1,7 +1,11 @@
 package com.choandyoo.jett.invitation.servie;
 
+import com.choandyoo.jett.chat.service.ChatService;
+import com.choandyoo.jett.config.CustomUserDetails;
 import com.choandyoo.jett.invitation.component.RedisService;
+import com.choandyoo.jett.travel.dto.request.TravelInviteRequest;
 import com.choandyoo.jett.travel.repository.TravelRepository;
+import com.choandyoo.jett.travel.service.TravelService;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,6 +18,8 @@ public class InvitationService {
     private static final String INVITE_LINK_PREFIX = "travelId=%d";
     private final RedisService redisService;
     private final TravelRepository travelRepository;
+    private final TravelService travelService;
+    private final ChatService chatService;
 
     @Transactional
     public String generateInvitation(Long travelId) {
@@ -33,4 +39,23 @@ public class InvitationService {
         return existingValue;
     }
 
+    @Transactional
+    public void respondToInvite(Long travelId, String invitation, CustomUserDetails customUserDetails) {
+        String validInvitation = redisService.getValues("travelId=" + travelId.toString());
+        Long userId = customUserDetails.getId();
+
+        boolean isPresent = travelRepository.findById(travelId).isPresent();
+        if(!isPresent) {
+            new RuntimeException("the travel do not exist.");
+        } else if (validInvitation == null || validInvitation.isEmpty()) {
+            new RuntimeException("Invalid or expired invitation");
+        } else if(validInvitation.equals(invitation)) {
+            new RuntimeException("travelId and invitation do not match.");
+        }
+        TravelInviteRequest travelInviteRequest = TravelInviteRequest.builder()
+                .email(customUserDetails.getUsername())
+                .build();
+        travelService.inviteTravel(travelInviteRequest, travelId);
+        chatService.addChatroomMember(userId, travelId);
+    }
 }
